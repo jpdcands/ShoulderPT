f290c1665077f00d4760b8c93d60d241bd5840c5
Paul <jpdcands211@gmail.com>
2024-01-02 15:23:50 -0500

    Changed name of MainActivity

--- app/src/main/java/com/example/shoulderpt/Welcome.kt
+++ app/src/main/java/com/example/shoulderpt/Welcome.kt
@@ -3,12 +3,9 @@ package com.example.shoulderpt
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.tooling.preview.Preview
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
@@ -16,8 +13,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -38,7 +33,7 @@ import com.example.shoulderpt.Pendulum
 import com.example.shoulderpt.PosteriorStretching
 
 
-class MainActivity : ComponentActivity() {
+class Welcome  : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {

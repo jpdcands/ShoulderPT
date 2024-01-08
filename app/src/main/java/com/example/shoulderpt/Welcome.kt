8ab82cc3aa0a184ddec6d4279fc56fa03ac1f359
Paul <jpdcands211@gmail.com>
2024-01-08 06:34:19 -0500

    Rounded corners image

--- app/src/main/java/com/example/shoulderpt/Welcome.kt
+++ app/src/main/java/com/example/shoulderpt/Welcome.kt
@@ -16,9 +16,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -112,7 +115,7 @@ fun MyApp() {
                     text = "Shoulder/Rotator",
                     modifier = Modifier
                         .align(Alignment.CenterHorizontally)
-                        .padding(top = 5.dp),
+                        .padding(top = 1.dp),
                     fontWeight = FontWeight.Bold,
                     style = TextStyle(
                         fontSize = 44.sp
@@ -122,23 +125,27 @@ fun MyApp() {
                     text = "Cuff Exercises",
                     modifier = Modifier
                         .align(Alignment.CenterHorizontally)
-                        .padding(vertical = 8.dp),
+                        .padding(top = 1.dp),
                     fontWeight = FontWeight.Bold,
                     style = TextStyle(
                         fontSize = 44.sp, // Font size for the second part
                         textAlign = TextAlign.Center // Centers the text within its composable
                     )
                 )
-           //     Spacer(modifier = Modifier.height(10.dp))
+                Spacer(modifier = Modifier.height(90.dp))
                 // LazyColumn for the list of exercises
              //   NamesLazyColumn(navController)
 
                 Image(
                     painter = painterResource(id = R.drawable.shoulderimage), // Replace with your image resource
-                    contentDescription = "Over the Head Stretch",
-                    modifier = Modifier.size(300.dp, 400.dp)
+                    modifier = Modifier
+                        .size(300.dp, 300.dp)
+                        .clip(RoundedCornerShape(26.dp)),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = "Shoulder Picture",
+
                 )
-           //     Spacer(modifier = Modifier.height(20.dp)) // Adjust the height as needed
+                Spacer(modifier = Modifier.height(40.dp)) // Adjust the height as needed
 
                 Box(
                     contentAlignment = Alignment.Center,
@@ -148,7 +155,7 @@ fun MyApp() {
                 ) {
               Column(horizontalAlignment = Alignment.CenterHorizontally) {
                   Button(onClick = { navController.navigate("HomePage") }) {
-                       Text("Begin Exercises", fontSize = 20.sp)
+                       Text("Begin Exercises", fontSize = 30.sp)
                         }
                     }
                 }

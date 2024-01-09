68de232d043c32452b09441b595b146cf94fa6d6
Paul <jpdcands211@gmail.com>
2024-01-09 07:16:18 -0500

    Start 1/9/24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,6 @@ android {
         implementation("androidx.compose.material3:material3")
         implementation("androidx.navigation:navigation-compose:2.7.6")
         implementation("androidx.compose.foundation:foundation-layout-android:1.5.4")
-        implementation "androidx.compose.material:material:$m2-version"
         testImplementation("junit:junit:4.13.2")
         androidTestImplementation("androidx.test.ext:junit:1.1.5")
         androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

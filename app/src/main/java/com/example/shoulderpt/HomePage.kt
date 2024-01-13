87d254dac02f3968b62a70f80c5b61ce70dd8135
Paul <jpdcands211@gmail.com>
2024-01-13 10:00:16 -0500

    New image on welcome page

--- app/src/main/java/com/example/shoulderpt/HomePage.kt
+++ app/src/main/java/com/example/shoulderpt/HomePage.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -139,6 +140,8 @@ fun NamesLazyColumn(navController: NavController) {
                             navController.navigate(route)
                         }
                     },
+                elevation = CardDefaults.cardElevation (
+                    defaultElevation = 10.dp)
             ) {
                 Image(
                     painter = painterResource(id = imageResource),

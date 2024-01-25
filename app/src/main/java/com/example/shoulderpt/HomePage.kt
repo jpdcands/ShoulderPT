581c9b6834368ae554247a656dbb15c1dbc92372
jpdcands <jpdcands211@gmail.com>
2024-01-25 16:43:29 -0500

    Images fill up Card

--- app/src/main/java/com/example/shoulderpt/HomePage.kt
+++ app/src/main/java/com/example/shoulderpt/HomePage.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -146,6 +147,7 @@ fun NamesLazyColumn(navController: NavController) {
                 Image(
                     painter = painterResource(id = imageResource),
                     contentDescription = name,
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
                         .fillMaxSize() // Fill the Card
                         .clip(RoundedCornerShape(4.dp)) // Clip the Image

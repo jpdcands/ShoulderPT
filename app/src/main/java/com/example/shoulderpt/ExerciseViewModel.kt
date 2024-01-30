6d88bd1f6a43ff40af72401d12f5390cd352c6fe
jpdcands <jpdcands211@gmail.com>
2024-01-30 06:25:04 -0500

    Start 1/30/23

--- app/src/main/java/com/example/shoulderpt/ExerciseViewModel.kt
+++ app/src/main/java/com/example/shoulderpt/ExerciseViewModel.kt
@@ -1,9 +1,8 @@
 package com.example.shoulderpt
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
+
 class ExerciseViewModel : ViewModel() {
 //    LiveData to hold the selected option
     var selectedOption = mutableStateOf("")
--- app/src/main/java/com/example/shoulderpt/Welcome.kt
+++ app/src/main/java/com/example/shoulderpt/Welcome.kt
@@ -1,49 +1,40 @@
 package com.example.shoulderpt
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.lifecycle.ViewModel
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-
-import androidx.compose.ui.unit.sp
 import com.example.shoulderpt.ui.theme.ShoulderPTTheme
-
-import com.example.shoulderpt.HomePage
-import com.example.shoulderpt.Pendulum
-import com.example.shoulderpt.PosteriorStretching
 
 
 class Welcome : ComponentActivity() {

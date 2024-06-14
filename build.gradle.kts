087987cb7284c4da218c92ee1f5d780c2553fc11
Paul Doolittle <jpdcands211@gmail.com>
2024-06-14 08:45:55 -0400

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 buildscript {
-    val agp_version by extra("8.2.2")
-    val agp_version1 by extra("8.2.2")
-    val agp_version2 by extra("8.3.0")
+    val agpVersion by extra("8.2.2")
+    val agpVersion1 by extra("8.2.2")
+    val agpVersion2 by extra("8.3.0")
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.1" apply false
+    id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }

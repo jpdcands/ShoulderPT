916af91d21a30bc83cd2a7920e2cc4dbc5baadbd
Paul Doolittle <jpdcands211@gmail.com>
2024-06-29 08:16:05 -0400

    Up-to-Date 6/29/24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
 
 android {
@@ -58,14 +59,14 @@ android {
         implementation("androidx.compose.ui:ui")
         implementation("androidx.compose.ui:ui-graphics")
         implementation("androidx.compose.ui:ui-tooling-preview")
-        implementation("androidx.compose.material3:material3:1.3.0-beta03")
-        implementation("androidx.navigation:navigation-compose:2.8.0-beta03")
-        implementation("androidx.compose.foundation:foundation-layout-android:1.7.0-beta03")
+        implementation("androidx.compose.material3:material3:1.3.0-beta04")
+        implementation("androidx.navigation:navigation-compose:2.8.0-beta04")
+        implementation("androidx.compose.foundation:foundation-layout-android:1.7.0-beta04")
         testImplementation("junit:junit:4.13.2")
-        androidTestImplementation("androidx.test.ext:junit:1.1.5")
-        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+        androidTestImplementation("androidx.test.ext:junit:1.2.1")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
         androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
         androidTestImplementation("androidx.compose.ui:ui-test-junit4")
         debugImplementation("androidx.compose.ui:ui-tooling")
-        debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.0-beta03")
+        debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.0-beta04")
     }
